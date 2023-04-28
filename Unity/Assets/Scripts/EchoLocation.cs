459784c6b4b2f94e4e8cf51ce3f9004ed4ce6d14
Luke Ryan <lukeryan1@virginmedia.com>
2023-04-28 17:24:03 +0100

    update

--- Unity/Assets/Scripts/EchoLocation.cs
+++ Unity/Assets/Scripts/EchoLocation.cs
@@ -1,9 +1,37 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class EchoLocation : MonoBehaviour
 {
+    private Controls controls;
+
+    private void Awake()
+    {
+        controls = new Controls();
+    }
+
+    private void OnEnable()
+    {
+        controls.Player.EchoLocal.performed += Local_Cast;
+        controls.Player.EchoFar.performed += End_Cast;
+    }
+
+    private void OnDisable()
+    {
+        controls.Player.EchoLocal.performed -= Local_Cast;
+        controls.Player.EchoFar.performed -= End_Cast;
+    }
+
+    private void Local_Cast(InputAction.CallbackContext ctx) 
+    {
+        print("local");
+    }
+
+    private void End_Cast(InputAction.CallbackContext ctx)
+    {
+        print("end");
+    }
+
     // Start is called before the first frame update
     void Start()
     {
